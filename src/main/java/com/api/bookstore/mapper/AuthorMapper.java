9596acbf2056dbe145cc8c0c665cea24091edce0
Ismayil <34961005+ismayilkarimli@users.noreply.github.com>
2021-10-30 02:38:04 +0400

    removed unnecessary imports

--- src/main/java/com/api/bookstore/mapper/AuthorMapper.java
+++ src/main/java/com/api/bookstore/mapper/AuthorMapper.java
@@ -1,9 +1,8 @@
 package com.api.bookstore.mapper;
 
 import com.api.bookstore.model.bean.Author;
-import com.api.bookstore.model.bean.Book;
 import com.api.bookstore.model.dto.AuthorDto;
-import com.api.bookstore.model.dto.BookDto;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
