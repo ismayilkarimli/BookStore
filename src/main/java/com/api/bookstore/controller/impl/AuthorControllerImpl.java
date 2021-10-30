3509313876c65ebf1bb11dd833d2ecf6ccb7da92
Ismayil <34961005+ismayilkarimli@users.noreply.github.com>
2021-10-30 13:52:22 +0400

    created endpoints in controllers with method stubs

--- src/main/java/com/api/bookstore/controller/impl/AuthorControllerImpl.java
+++ src/main/java/com/api/bookstore/controller/impl/AuthorControllerImpl.java
@@ -1,8 +1,47 @@
 package com.api.bookstore.controller.impl;
 
 import com.api.bookstore.controller.AuthorController;
-import org.springframework.stereotype.Controller;
+import com.api.bookstore.model.dto.AuthorDto;
+import com.api.bookstore.service.AuthorService;
+import lombok.RequiredArgsConstructor;
 
-@Controller
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/author")
+@RequiredArgsConstructor
 public class AuthorControllerImpl implements AuthorController {
+
+    private final AuthorService authorService;
+
+    @PostMapping("/add")
+    public ResponseEntity<HttpStatus> addAuthor(@Valid @RequestBody AuthorDto authorDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HttpStatus> searchAuthorById(@PathVariable("id") UUID authorId) {
+        return ResponseEntity.status(HttpStatus.OK).body(null); // body will contain author
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<HttpStatus> searchAuthorsByName(@RequestParam("name") String name) {
+        return ResponseEntity.status(HttpStatus.OK).body(null); // body will contain author(s)
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<HttpStatus> updateAuthor(@PathVariable("id") UUID authorId) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null); // body will contain updated author
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteAuthor(@PathVariable("id") UUID authorId) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
--- src/main/java/com/api/bookstore/controller/impl/BookControllerImpl.java
+++ src/main/java/com/api/bookstore/controller/impl/BookControllerImpl.java
@@ -1,7 +1,49 @@
 package com.api.bookstore.controller.impl;
 
-import org.springframework.stereotype.Controller;
+import com.api.bookstore.model.bean.Book;
+import com.api.bookstore.model.dto.BookDto;
+import com.api.bookstore.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@Controller
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/book")
+@RequiredArgsConstructor
 public class BookControllerImpl {
+
+    private final BookService bookService;
+
+    @PostMapping("/add")
+    public ResponseEntity<HttpStatus> addBook(@Valid @RequestBody BookDto bookDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> searchBookById(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(null); // body will contain book
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> searchBooksByName(@RequestParam String name) {
+        return ResponseEntity.status(HttpStatus.OK).body(null); // body will contain book(s)
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<HttpStatus> updateBook(@PathVariable("id") UUID bookId,
+                                                 @Valid @RequestBody BookDto bookDto) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null); // body will contain updated book
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteBook(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
 }
