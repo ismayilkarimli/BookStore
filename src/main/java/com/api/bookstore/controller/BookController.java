601f58c0696cdd163dd78d5230fc7593faf09a82
Ismayil <34961005+ismayilkarimli@users.noreply.github.com>
2021-10-30 18:49:36 +0400

    added methods to the controller interfaces

--- src/main/java/com/api/bookstore/controller/BookController.java
+++ src/main/java/com/api/bookstore/controller/BookController.java
@@ -1,4 +1,25 @@
 package com.api.bookstore.controller;
 
+import com.api.bookstore.model.dto.BookDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.List;
+
 public interface BookController {
+
+    ResponseEntity<HttpStatus> addBook(BookDto bookDto);
+
+    ResponseEntity<BookDto> searchBookById(Long id);
+
+    ResponseEntity<List<BookDto>> searchBooksByTitle(String title);
+
+    ResponseEntity<BookDto> updateBook(Long bookId, BookDto bookDto);
+
+    ResponseEntity<HttpStatus> deleteBook(Long id);
+
 }
