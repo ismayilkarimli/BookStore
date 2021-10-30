2d331d3eec4ef83c8973dd092cde8a2a82d9b30c
Ismayil <34961005+ismayilkarimli@users.noreply.github.com>
2021-10-30 17:55:31 +0400

    implemented some service methods

--- src/main/java/com/api/bookstore/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/api/bookstore/service/impl/AuthorServiceImpl.java
@@ -1,9 +1,13 @@
 package com.api.bookstore.service.impl;
 
+import com.api.bookstore.mapper.AuthorMapper;
+import com.api.bookstore.model.bean.Author;
 import com.api.bookstore.model.dto.AuthorDto;
 import com.api.bookstore.repository.AuthorRepository;
 import com.api.bookstore.service.AuthorService;
 import lombok.RequiredArgsConstructor;
+
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,12 +21,17 @@ public class AuthorServiceImpl implements AuthorService {
 
     @Override
     public void addAuthor(AuthorDto authorDto) {
-
+        Author author = AuthorMapper.INSTANCE.authorDtoToAuthor(authorDto);
+        authorRepository.save(author);
     }
 
     @Override
     public AuthorDto searchAuthorById(UUID authorId) {
-        return null;
+        Author author = authorRepository.findById(authorId)
+                .orElseThrow(() -> new RuntimeException("no such author"));
+        AuthorDto authorDto = AuthorMapper.INSTANCE.authorToAuthorDto(author);
+
+        return authorDto;
     }
 
     @Override
@@ -32,11 +41,19 @@ public class AuthorServiceImpl implements AuthorService {
 
     @Override
     public AuthorDto updateAuthor(UUID authorId, AuthorDto dto) {
-        return null;
+        Author author = authorRepository.findById(authorId)
+                .orElseThrow(() -> new RuntimeException("no such author"));
+        author.setName(dto.name());
+        author.setLastName(dto.lastName());
+
+        Author updatedAuthor = authorRepository.save(author);
+        AuthorDto authorDto = AuthorMapper.INSTANCE.authorToAuthorDto(updatedAuthor);
+
+        return authorDto;
     }
 
     @Override
     public void deleteAuthor(UUID id) {
-
+        authorRepository.deleteById(id);
     }
 }
--- src/main/java/com/api/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/api/bookstore/service/impl/BookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.api.bookstore.service.impl;
 
+import com.api.bookstore.mapper.BookMapper;
+import com.api.bookstore.model.bean.Book;
 import com.api.bookstore.model.dto.BookDto;
 import com.api.bookstore.repository.BookRepository;
 import com.api.bookstore.service.BookService;
@@ -17,12 +19,17 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public void addBook(BookDto bookDto) {
-
+        Book book = BookMapper.INSTANCE.bookDtoToBook(bookDto);
+        bookRepository.save(book);
     }
 
     @Override
     public BookDto searchBookById(UUID bookId) {
-        return null;
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new RuntimeException("no such book"));
+        BookDto dto = BookMapper.INSTANCE.bookToBookDto(book);
+
+        return dto;
     }
 
     @Override
@@ -32,11 +39,19 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto updateBook(UUID bookId, BookDto dto) {
-        return null;
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new RuntimeException("no such book"));
+        book.setName(dto.name());
+        book.setPageCount(dto.pageCount());
+        book.setReleaseDate(dto.releaseDate());
+        Book savedBook = bookRepository.save(book);
+        BookDto bookDto = BookMapper.INSTANCE.bookToBookDto(savedBook);
+
+        return bookDto;
     }
 
     @Override
     public void deleteBook(UUID id) {
-
+        bookRepository.deleteById(id);
     }
 }
