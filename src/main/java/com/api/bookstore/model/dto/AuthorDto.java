7fb539ac31728ad7c1fbf3ef1943d829a962542d
Ismayil <34961005+ismayilkarimli@users.noreply.github.com>
2021-11-04 03:25:45 +0400

    added pattern validation to author

--- src/main/java/com/api/bookstore/model/dto/AuthorDto.java
+++ src/main/java/com/api/bookstore/model/dto/AuthorDto.java
@@ -7,15 +7,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 import java.util.Set;
 
 public record AuthorDto(
         @NotBlank(message = "author name cannot be blank")
+        @Pattern(regexp = "^[A-Za-z-' ]*$", message = "author name can only include letters, dash, and apostrophe")
         String name,
 
-        @JsonProperty("lastname")
+        @JsonProperty("lastName")
         @NotBlank(message = "author last name cannot be blank")
+        @Pattern(regexp = "^[A-Za-z-' ]*$", message = "author last name can only include letters, dash, and apostrophe")
         String lastName,
 
         @JsonIgnoreProperties({ "authors", "createdAt", "updatedAt" })
