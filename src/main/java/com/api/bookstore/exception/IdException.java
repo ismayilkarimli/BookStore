e09f175ee76e950140fcab4d3dcb1516fe20cfc8
Ismayil <34961005+ismayilkarimli@users.noreply.github.com>
2021-11-03 18:17:45 +0400

    added model for error response and exception handler

--- src/main/java/com/api/bookstore/exception/IdException.java
+++ src/main/java/com/api/bookstore/exception/IdException.java
@@ -3,10 +3,11 @@ package com.api.bookstore.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource with the requested ID does not exist")
 public class IdException extends RuntimeException {
 
     public IdException(String message) {
         super(message);
     }
+
 }
---
+++ src/main/java/com/api/bookstore/model/error/ApiError.java
@@ -0,0 +1,14 @@
+package com.api.bookstore.model.error;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiError(String errorTitle, Map<String, String> errors) {
+
+    public ApiError(String errorTitle) {
+        this(errorTitle, null);
+    }
+
+}
