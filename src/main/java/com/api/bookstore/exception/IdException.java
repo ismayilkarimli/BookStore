507d291598641dbf921edb055e483f15ebab5d80
Ismayil <34961005+ismayilkarimli@users.noreply.github.com>
2021-11-03 16:13:01 +0400

    replaced RuntimeExceptions with custom IdException

---
+++ src/main/java/com/api/bookstore/exception/IdException.java
@@ -0,0 +1,12 @@
+package com.api.bookstore.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class IdException extends RuntimeException {
+
+    public IdException(String message) {
+        super(message);
+    }
+}
